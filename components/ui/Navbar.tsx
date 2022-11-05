8ae71f52af3de5dcedab53786efd0a4f19e8a3de
José Balboa <joselimberg291197@gmail.com>
2022-11-04 19:08:51 -0700

    SWR agregado, busquedas de productos agregadas, UIContext agregado, base de datos actualizada

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,6 @@
+import { useContext, useState } from "react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 import {
   AppBar,
@@ -6,57 +8,121 @@ import {
   Box,
   Button,
   IconButton,
-  Link as MuiLink,
+  Input,
+  InputAdornment,
+  Link,
   Toolbar,
   Typography,
 } from "@mui/material";
-import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+import {
+  ClearOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+
+import { UiContext } from "../../context";
 
 export const Navbar = () => {
+  const { asPath, push } = useRouter();
+  const { toggleSideMenu } = useContext(UiContext);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+  const onSearchTerm = () => {
+    if (searchTerm.trim().length === 0) return;
+    push(`/search/${searchTerm}`);
+  };
+
   return (
     <AppBar>
       <Toolbar>
-        <MuiLink
+        <Link
+          component={NextLink}
           display="flex"
           alignItems="center"
-          component={NextLink}
           href="/"
+          passHref
         >
           <Typography variant="h6">Teslo |</Typography>
           <Typography sx={{ ml: 0.5 }}>Shop</Typography>
-        </MuiLink>
+        </Link>
 
         <Box flex={1} />
 
-        <Box sx={{ display: { xs: "none", sm: "block" } }}>
-          <MuiLink component={NextLink} href="/category/men" passHref>
-            <Button>Hombres</Button>
-          </MuiLink>
+        <Box
+          sx={{
+            display: isSearchVisible ? "none" : { xs: "none", sm: "block" },
+          }}
+          className="fadeIn"
+        >
+          <Link component={NextLink} href="/category/men" passHref>
+            <Button color={asPath === "/category/men" ? "primary" : "info"}>
+              Hombres
+            </Button>
+          </Link>
 
-          <MuiLink component={NextLink} href="/category/women" passHref>
-            <Button>Mujeres</Button>
-          </MuiLink>
+          <Link component={NextLink} href="/category/women" passHref>
+            <Button color={asPath === "/category/women" ? "primary" : "info"}>
+              Mujeres
+            </Button>
+          </Link>
 
-          <MuiLink component={NextLink} href="/category/kid" passHref>
-            <Button>Niños</Button>
-          </MuiLink>
+          <Link component={NextLink} href="/category/kid" passHref>
+            <Button color={asPath === "/category/kid" ? "primary" : "info"}>
+              Niños
+            </Button>
+          </Link>
         </Box>
 
         <Box flex={1} />
 
-        <IconButton>
+        {/* Pantallas pantallas grandes */}
+        {isSearchVisible ? (
+          <Input
+            sx={{ display: { xs: "none", sm: "flex" } }}
+            className="fadeIn"
+            autoFocus
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={(e) => (e.key === "Enter" ? onSearchTerm() : null)}
+            type="text"
+            placeholder="Buscar..."
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton onClick={() => setIsSearchVisible(false)}>
+                  <ClearOutlined />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        ) : (
+          <IconButton
+            onClick={() => setIsSearchVisible(true)}
+            className="fadeIn"
+            sx={{ display: { xs: "none", sm: "flex" } }}
+          >
+            <SearchOutlined />
+          </IconButton>
+        )}
+
+        {/* Pantallas pequeñas */}
+        <IconButton
+          sx={{ display: { xs: "flex", sm: "none" } }}
+          onClick={toggleSideMenu}
+        >
           <SearchOutlined />
         </IconButton>
 
-        <MuiLink component={NextLink} href="/cart" passHref>
+        <Link component={NextLink} href="/cart" passHref>
           <IconButton>
             <Badge badgeContent={2} color="secondary">
               <ShoppingCartOutlined />
             </Badge>
           </IconButton>
-        </MuiLink>
+        </Link>
 
-        <Button>Menú</Button>
+        <Button onClick={toggleSideMenu}>Menú</Button>
       </Toolbar>
     </AppBar>
   );
